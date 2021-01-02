9f1c7aafc45844b517ba15791ff5e7ecde145a3e
Achuth Karakkat <achuth2000@hotmail.com>
2021-01-02 17:57:36 +0530

    Fixes

--- js/app.js
+++ js/app.js
@@ -5,11 +5,28 @@ let sh =  screen.height;
 let duration = 4.202625;
 let unit = duration/sw;
 
-const track = (e)=> {
- var x = e.clientX;
-  var y = e.clientY;
-  var coor = "Coordinates: (" + x + "," + y + ")";
-  vid.currentTime = unit*x;
-  console.log(coor);
+window.addEventListener('mousemove',(e)=>{
+    console.log(e);
+    let x = e.clientX;
+    let y = e.clientY;
+    seekToTime(unit*x);
+})
+
+const updateTime = (x)=> {
+    vid.currentTime = unit*x;
 }
 
+
+function seekToTime(ts) {
+  // try and avoid pauses after seeking
+  vid.pause();
+  vid.currentTime = ts; // if this is far enough away from current, it implies a "play" call as well...oddly. I mean seriously that is junk.
+    // however if it close enough, then we need to call play manually
+    // some shenanigans to try and work around this:
+    var timer = setInterval(function() {
+        if (vid.paused && vid.readyState ==4 || !vid.paused) {
+            vid.play();
+            clearInterval(timer);
+        }       
+    }, 50);
+}
